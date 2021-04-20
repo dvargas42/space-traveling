d6f7e90bbce862beb3ffe3dc6ee60c4bb4a6dc0a
dvargas42 <dvargas.eng@gmail.com>
2021-04-20 20:13:54 -0300

    home struture creation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom';
+import { format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { FiCalendar, FiClock } from 'react-icons/fi'
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +30,93 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+      <main className={styles.container}>
+        <div className={styles.postList}>
+          <img src="./images/logo.svg" alt="logo"/>
+          <a className={styles.post} href="#">
+            <h1>Como utilizar Hooks</h1>
+            <strong>Pensando em sincronização em vez de ciclos de vida</strong>
+            <section>
+              <time><FiCalendar />19 Abr 2021</time>
+              <p><FiClock />Danilo Vieira</p>
+            </section>
+          </a>
+          <a className={styles.post} href="#">
+            <h1>Como utilizar Hooks</h1>
+            <strong>Pensando em sincronização em vez de ciclos de vida</strong>
+            <section>
+              <time><FiCalendar />19 Abr 2021</time>
+              <p><FiClock />Danilo Vieira</p>
+            </section>
+          </a>
+          <a className={styles.post} href="#">
+            <h1>Como utilizar Hooks</h1>
+            <strong>Pensando em sincronização em vez de ciclos de vida</strong>
+            <section>
+              <time><FiCalendar />19 Abr 2021</time>
+              <p><FiClock />Danilo Vieira</p>
+            </section>
+          </a>
+          <a className={styles.post} href="#">
+            <h1>Como utilizar Hooks</h1>
+            <strong>Pensando em sincronização em vez de ciclos de vida</strong>
+            <section>
+              <time><FiCalendar />19 Abr 2021</time>
+              <p><FiClock />Danilo Vieira</p>
+            </section>
+          </a>
+          <a className={styles.post} href="#">
+            <h1>Como utilizar Hooks</h1>
+            <strong>Pensando em sincronização em vez de ciclos de vida</strong>
+            <section>
+              <time><FiCalendar />19 Abr 2021</time>
+              <p><FiClock />Danilo Vieira</p>
+            </section>
+          </a>
+          <a className={styles.nextPage}>Carregar mais posts</a>
+        </div>
+      </main>
+    </>
+  )
+}
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const postsResponse = await prismic.query(
+    Prismic.Predicates.at('document.type', 'post'),
+    {
+      fetch: ['post.title', 'post.subtitle'],
+      pageSize: 3,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(new Date(post.last_publication_date),
+        'dd MMM yyyy',
+        { locale: ptBR }
+      ),
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: RichText.asText(post.data.subtitle),
+        author: RichText.asText(post.data.author)
+      },
+    }
+  })
+
+  return {
+    props: {
+      posts
+    },
+    revalidate: 60 * 60 // 1 hour
+  }
+};
