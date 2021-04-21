3c36b4f3ebd1c3f865106e18c990996733e305c1
dvargas42 <dvargas.eng@gmail.com>
2021-04-21 01:46:13 -0300

    creation of the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
+import styles from './header.module.scss'
+
 export default function Header() {
-  // TODO
+  return(
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo"/>
+      </div>
+    </header>
+
+  )
 }
