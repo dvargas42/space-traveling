c146880b76a11ba0ecec6f8a11a8c5084fed34a7
dvargas42 <dvargas.eng@gmail.com>
2021-04-30 19:55:00 -0300

    chaging logic

--- src/components/PreviewButton/index.tsx
+++ src/components/PreviewButton/index.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 import styles from './styles.module.scss'
 
 export function PreviewButton() {
-  return( true && (
+  return(
     <aside className={styles.container}>
       <Link href="/api/exit-preview">
         <a className={styles.content}>Sair do modo Preview</a>
       </Link>
     </aside>
-  ))
+  )
 }
